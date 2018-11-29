81bf6cbb41ababd7921000af090cf363d7d10f66
Karla Joselly Ruiz <karla.joselly@gmail.com>
2018-11-29 18:51:51 -0500

    Updates

---
+++ sketch.js
@@ -0,0 +1,16 @@
+// Project 1
+// Karla Ruiz
+// Drawing Machine
+
+
+function setup() {
+    
+//Set canvas to be the whole window    
+  createCanvas(windowWidth, windowHeight);
+
+
+}
+function draw() {
+    //set an opacity to the background so that you can still see the path of 
+}
+
